13c81103838d598fc072671d3ee93bd69f4ffc6e
Ivan <ikuznets93@gmail.com>
2026-04-15 00:57:43 +0300

    add db

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,22 @@
 import os
+import psycopg2
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+    )
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
+
+
+conn = psycopg2.connect(app.config['DATABASE_URL'])
+#repo = UserRepository(conn)
 
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
