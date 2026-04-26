720a1084b0834138861ceca973080d53290e73ac
Ivan <ikuznets93@gmail.com>
2026-04-26 19:00:49 +0300

    fix html parser

--- page_analyzer/html_parser.py
+++ page_analyzer/html_parser.py
@@ -5,16 +5,16 @@ def parser(response):
     soup = BeautifulSoup(response.text, "html.parser")
     result = {}
  
-    h1 = soup.h1.string if soup.h1 else None
-    title = soup.title.string if soup.title else None
+    h1 = soup.h1.string[:255] if soup.h1 else None
+    title = soup.title.string[:255] if soup.title else None
     meta_tag = soup.find("meta", attrs={"name": "description"})
     if meta_tag:
-        meta_description = meta_tag.get("content")
+        meta_description = meta_tag.get("content")[:255]
     else:
         meta_description = None
     
-    result["h1"] = h1[:255]
-    result["title"] = title[:255]
-    result["description"] = meta_description[:255]
+    result["h1"] = h1
+    result["title"] = title
+    result["description"] = meta_description
     
     return result
