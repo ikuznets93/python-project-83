6ec458583b654ecf5d11dbc6f05cd568cf40101a
Ivan <ikuznets93@gmail.com>
2026-04-26 16:59:36 +0300

    fix sql query

--- page_analyzer/url_repository.py
+++ page_analyzer/url_repository.py
@@ -74,7 +74,7 @@ class UrlRepository:
         FROM urls
         LEFT JOIN url_checks ON 
             urls.id = url_checks.url_id
-        ORDER BY id, created_at DESC;
+        ORDER BY urls.id, url_checks.id DESC;
         '''
         with self.conn.cursor(cursor_factory=RealDictCursor) as cur:
             cur.execute(query)
