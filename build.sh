2265b2924d109bd8fcc5a804ebd4c8700706c917
Ivan <ikuznets93@gmail.com>
2026-04-26 20:18:29 +0300

    add psql installation to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+set -e
 
+apt-get update
+apt-get install -y postgresql-client
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export PATH="$HOME/.local/bin:$PATH"
-make install && psql -a -d $DATABASE_URL -f database.sql
+make install
+psql -a -d "$DATABASE_URL" -f database.sql
